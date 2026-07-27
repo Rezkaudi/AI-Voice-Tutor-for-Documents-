d72f2d1d0572c6ec5afbd6f392bde19cb67f614c
Rezkaudi2002 <rezkaudi2002@gmail.com>
2026-07-27 19:30:23 +0300

    refactor: remove unused UserMenu component and associated AppHeader references

--- front-end/src/components/layout/AppHeader.tsx
+++ front-end/src/components/layout/AppHeader.tsx
@@ -1,13 +1,9 @@
 import { useTranslation } from "react-i18next";
 import { useDocumentStore } from "@/store/documentStore";
-// import { useAuthStore } from "@/store/authStore";
-// import { UserMenu } from "@/components/common/UserMenu";
 import { LanguageSwitcher } from "@/components/common/LanguageSwitcher";
 
 export function AppHeader() {
   const loadedDocument = useDocumentStore((s) => s.loadedDocument);
-
-  // const user = useAuthStore((s) => s.user);
   const { t } = useTranslation();
 
   return (
@@ -24,7 +20,6 @@ export function AppHeader() {
       </div>
       <div className="flex flex-none items-center justify-end gap-2">
         <LanguageSwitcher />
-        {/* {user && <UserMenu user={user} />} */}
       </div>
     </header>
   );
